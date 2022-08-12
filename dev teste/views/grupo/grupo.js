9634886e77cebe723837b80aef689180cfc03622
Drogendoy <101200223+JulioClaudino@users.noreply.github.com>
2022-08-12 18:02:01 -0300

    estudar

--- dev teste/views/grupo/grupo.js
+++ dev teste/views/grupo/grupo.js
@@ -1,22 +1,25 @@
-var valorInput = '';
 var grupos = [];
+var valorInput = '';
+var editarBanco = '';
+var valorAntigo = '';
 $(document).ready(function () {
-    html = '';
+    var html = '';
     if (window.localStorage.getItem('grupo')) {
         grupos = JSON.parse(window.localStorage.getItem('grupo'));
         grupos.map((val) => {
-            html += '<div id="' + val.grupo + '" class="list-group" id="list-tab" role="tablist">' +
+            valorInput = create_UUID();
+            html += '<div id="' + valorInput + '" class="list-group" id="list-tab" role="tablist">' +
                 '<div class="row justify-content-center">' +
                 '<div class="col-5">' +
                 '<div class="input-group mb-3">' +
-                '<input id="' + val.grupo + '" class="form-control" value="' + val.grupo + '" disabled>' +
+                '<input id="campoId_' + valorInput + '" class="form-control" value="' + val.grupo + '" disabled>' +
                 '<div class="col-1">' +
                 '<div id="buttonEditar" class="input-group-append">' +
-                '<button class="btn btn-outline-primary editar" type="button"><i class="las la-pen" style="font-size: 25px"></i></button>' +
+                '<button class="btn btn-outline-primary editar" editarBanco="' + valorInput + '" type="button"><i class="las la-pen" style="font-size: 25px"></i></button>' +
                 '</div></div>' +
                 '<div class="col-1 offset-md-1">' +
                 '<div id="buttonCancelar" class="input-group-append">' +
-                '<button class="btn btn-outline-danger remover" removerDiv="' + val.grupo + '" type="button"><i class="las la-trash-alt" style="font-size: 25px"></i></button>' +
+                '<button class="btn btn-outline-danger remover" removerDiv="' + valorInput + '" type="button"><i class="las la-trash-alt" style="font-size: 25px"></i></button>' +
                 '</div></div></div></div></div></div>'
         });
         html += '<div id="buttonAdicionar" class="col-md-6 offset-md-8"><button class="btn btn-outline-primary adicionar" type="button">' +
@@ -38,11 +41,11 @@ $(document).ready(function () {
             '<input id="campo" class="form-control">' +
             '<div class="col-1">' +
             '<div id="buttonEditar" class="input-group-append">' +
-            '<button class="btn btn-success confirmar" type="button"><i class="lar la-check-circle" style="font-size: 25px"></i></button>' +
+            '<button class="btn btn-success confirmar" editarBanco="teste" type="button"><i class="lar la-check-circle" style="font-size: 25px"></i></button>' +
             '</div></div>' +
             '<div class="col-1 offset-md-1">' +
             '<div id="buttonCancelar" class="input-group-append">' +
-            '<button class="btn btn-danger cancelar" type="button"><i class="las la-ban" style="font-size: 25px"></button>' +
+            '<button class="btn btn-danger cancelar" removerDiv="teste" type="button"><i class="las la-ban" style="font-size: 25px"></button>' +
             '</div></div></div></div></div></div>');
     });
 
@@ -52,50 +55,76 @@ $(document).ready(function () {
     });
 
     $('#adicionarDiv').on('click', '.confirmar', function () {
+        var valorConfirmar = $("#campo").val();
         if ($('#campo').val() == '') {
             alert('Por favor preencha o campo');
         } else {
-            if ($(this).is(".btn-success")) {
-                $(this).html('<i class="las la-pen" style="font-size: 25px"></i>');
-                $(this).toggleClass('btn-success btn-outline-primary');
-                $(this).toggleClass('confirmar editar');
-                $(".cancelar").html('<i class="las la-trash-alt" style="font-size: 25px"></i>');
-                $(".cancelar").toggleClass('btn-danger btn-outline-danger');
-                $(".cancelar").toggleClass('cancelar remover');
-                $("#campo").prop('disabled', true);
-                $('.adicionar').removeAttr("style");
-                valorInput = $('#campo').val();
-                grupos.push({ 'grupo': valorInput });
-                window.localStorage.setItem('grupo', JSON.stringify(grupos));
-                alert(valorInput);
+            var control = true
+            grupos.map((val) => {
+                debugger;
+                if (val.grupo == valorConfirmar) {
+                    alert("Esse grupo já existe");
+                    control = false
+                }
+            });
+            if (control) {
+                if ($(this).is(".btn-success")) {
+                    valorInput = create_UUID();
+                    $(this).html('<i class="las la-pen" style="font-size: 25px"></i>');
+                    $(this).toggleClass('btn-success btn-outline-primary');
+                    $(this).toggleClass('confirmar editar');
+                    $(this).attr('editarBanco', valorInput);
+                    $(".cancelar").html('<i class="las la-trash-alt" style="font-size: 25px"></i>');
+                    $(".cancelar").attr('removerDiv', valorInput);
+                    $(".cancelar").toggleClass('btn-danger btn-outline-danger');
+                    $(".cancelar").toggleClass('cancelar remover');
+                    $("#campo").prop('disabled', true);
+                    $('.adicionar').removeAttr("style");
+                    $("#excluir").attr('id', valorInput);
+                    $("#campo").attr('id', 'campoId_' + valorInput);
+                    grupos.push({ 'grupo': $('#campoId_' + valorInput).val() });
+                    window.localStorage.setItem('grupo', JSON.stringify(grupos));
+                }
             }
         }
     });
 
     $('#adicionarDiv').on('click', '.editar', function () {
-        if ($('#campo').val() == '') {
+        editarBanco = $(this).attr('editarBanco');
+        var valorInputLocal = $("#campoId_" + editarBanco).val();
+        if ($("#campoId_" + editarBanco).val() == '') {
             alert('Por favor preencha o campo');
         } else {
             if ($(this).is(".btn-outline-primary")) {
                 $(this).html('<i class="las la-check-double" style="font-size: 25px"></i>');
                 $(this).toggleClass('btn-outline-primary btn-outline-success');
-                $("#campo").prop('disabled', false);
+                $("#campoId_" + editarBanco).prop('disabled', false);
             } else {
+                var i = 0;
                 $(this).html('<i class="las la-pen" style="font-size: 25px"></i>');
                 $(this).toggleClass('btn-outline-success btn-outline-primary');
-                $("#campo").prop('disabled', true);
+                $("#campoId_" + editarBanco).prop('disabled', true);
+                window.localStorage.clear();
+                grupos.map((val) => {
+                    if (val.grupo == valorAntigo) {
+                        grupos[i].grupo = valorInputLocal;
+                    }
+                    i++;
+                });
+                window.localStorage.setItem('grupo', JSON.stringify(grupos));
             }
         }
+        valorAntigo = $("#campoId_" + editarBanco).val();
     });
 
     $('#adicionarDiv').on('click', '.remover', function () {
-        debugger;
         var i = 0;
         var removerDiv = $(this).attr('removerDiv');
+        valorInput = $("#campoId_" + removerDiv).val();
         window.localStorage.clear();
         $('#' + removerDiv).remove();
         grupos.map((val) => {
-            if (val.grupo == removerDiv) {
+            if (val.grupo == valorInput) {
                 grupos.splice([i], 1)
             }
             i++;
@@ -103,3 +132,13 @@ $(document).ready(function () {
         window.localStorage.setItem('grupo', JSON.stringify(grupos));
     });
 });
+
+function create_UUID() {
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
+}
--- dev teste/views/permissao/permissao.js
+++ dev teste/views/permissao/permissao.js
@@ -1,291 +1,55 @@
-var dataVer = true;
-if (dataVer) {
-    data = [{
-        grupo: "TI",
-        permissao: [
-            "editar", "excluir", "incluir", "visualizar"
-        ]
-    },
-    {
-        grupo: "Contabilidade",
-        permissao: [
-            "visualizar"
-        ]
-    },
-    {
-        grupo: "Diretoria",
-        permissao: [
-            "editar", "incluir", "visualizar"
-        ]
-    }];
-    dataVer = false;
-}
-var valorAnterior = '';
-var grupo_Valor = '';
-var selecionado = '';
-var valAntQue = '';
-var testeArray = '';
-var valorData = '';
-var selectId = '';
-var valorAnteriorArray = '';
+var grupos = [];
+var UUID = '';
 $(document).ready(function () {
     var html = '';
-    var poderes = '';
-    var control = true;
+    if (window.localStorage.getItem('grupo')) {
+        var html = '';
+        var poderes = '';
+        grupos = JSON.parse(window.localStorage.getItem('grupo'));
+        var control = true;
 
-    data.map((val) => {
-        html += '<a style="" class="list-group-item list-group-item-action bloquearLink' + (control ? ' active' : '') + '"' +
-            'id="list-' + val.grupo + '-list" data-toggle="list"' +
-            'href="#list-' + val.grupo + '" role="tab" aria-controls="' + val.grupo + '">' + val.grupo + '</a>'
-        control = false;
-    });
-    $('#list-tab').html(html);
+        grupos.map((val) => {
+            UUID = create_UUID();
+            html += '<a style="" class="list-group-item list-group-item-action bloquearLink' + (control ? ' active' : '') + '"' +
+                'id="grupos_' + UUID + '-list" data-toggle="list"' +
+                'href="#list-' + UUID + '" role="tab" aria-controls="' + UUID + '">' + val.grupo + '</a>'
 
-    control = true;
-    data.map((val) => {
-        poderes += '<div class="tab-pane fade show' + (control ? ' active' : '') + '" id="list-' + val.grupo + '"' +
-            'role="tabpanel" aria-labelledby="list-' + val.grupo + '-list">'
-        val.permissao.map((val2) => {
-            poderes += '<div class="row ' + val.grupo + '_' + val2 + '"><div class="col-5">' +
-                '<select id="' + val.grupo + '_' + val2 + '" class="form-control" name="' + val.grupo + '_' + val2 + '" disabled>' +
-                '<option value="editar" ' + (val2 === "editar" ? 'selected' : '') + '>editar</option>' +
-                '<option value="excluir" ' + (val2 === "excluir" ? 'selected' : '') + '>excluir</option>' +
-                '<option value="incluir" ' + (val2 === "incluir" ? 'selected' : '') + '>incluir</option>' +
-                '<option value="visualizar" ' + (val2 === "visualizar" ? 'selected' : '') + '>visualizar</option>' +
-                '</select></div >' +
-                '<div class="col-1 botaoum">' +
-                '<button class="btn btn-outline-primary editar" valor="' + val.grupo + '_' + val2 + '" type="button data-toggle="tooltip" data-placement="top" title="Deletar opção ' + val2 + '""><i class="las la-pen" style="font-size: 25px"></i></button>' +
-                '</div>' +
-                '<div class="col-1 botaodois">' +
-                '<button class="btn btn-outline-danger remover ' + val.grupo + '_' + val2 + '" valorRemover="' + val.grupo + '_' + val2 + '" adicionar="list-' + val.grupo + '" remover="' + val.grupo + '" bloquearlista="list-' + val.grupo + '-list" type="button data-toggle="tooltip" data-placement="top" title="Deletar opção ' + val2 + '""><i class="las la-trash-alt" style="font-size: 25px"></i></button>' +
-                '</div></div>';
-        })
-        poderes += '<div class="col-md-4 offset-md-6">' +
-            '<button class="btn btn-outline-primary adicionar" adicionar="list-' + val.grupo + '" id="adicionar_' + val.grupo + '" type="button data-toggle=" tooltip="" data-placement="top" title="Deletar opção visualizar"> <i class="las la-plus" style="font-size: 25px"></i></button>' +
-            '</div>'
-        poderes += '</div>';
-        control = false;
-    })
-    $('#nav-tabContent').html(poderes);
-
-    $('.tab-content').on('click', '.editar', function (event) {
-        $('#list-tab a').each(function (index) {
-            if ($(this).hasClass('active')) {
-                grupo_Valor = $(this).text();
-            }
-        });
-        if ($(this).is(".btn-outline-primary")) {
-            valorAnterior = $(this).attr("valor");
-            $(this).html('<i class="las la-check-double" style="font-size: 25px"></i>');
-            $(this).toggleClass('btn-outline-primary btn-outline-success');
-            $("#" + valorAnterior).prop('disabled', false);
-        } else {
-            debugger;
-            var i = 0;
-            var j = 0;
-            valAntQue = valorAnterior.split("_");
-            var confirmar = $("#" + valorAnterior + " option:selected").val();
-            $(this).html('<i class="las la-pen" style="font-size: 25px"></i>');
-            $(this).toggleClass('btn-outline-success btn-outline-primary');
-            $("#" + valorAnterior).prop('disabled', true);
-            data.map((val) => {
-                if (val.grupo == valAntQue[0]) {
-                    val.permissao.map((val2) => {
-                        if (val2 == valAntQue[1]) {
-                            data[i].permissao[j] = confirmar;
-                        }
-                        j++;
-                    })
-                }
-                i++;
-            });
-            console.log(data);
-            $('#' + valorAnterior).attr('id', '' + grupo_Valor + '_' + selecionado + '');
-            $(this).attr('valor', '' + grupo_Valor + '_' + selecionado + '');
-            $('.' + valorAnterior).attr('valorRemover', '' + grupo_Valor + '_' + selecionado + '');
-            $('.' + valorAnterior).toggleClass(valorAnterior + ' ' + grupo_Valor + '_' + selecionado);
-            valorAnterior = $(this).attr("valor");
-        }
-    });
-
-    $('.remover').click(function () {
-        var i = 0;
-        var j = 0;
-        valorAnterior = $(this).attr("valorRemover");
-        valAntQue = valorAnterior.split("_");
-        $('.' + valorAnterior).remove();
-        data.map((val) => {
-            if (val.grupo == valAntQue[0]) {
+            if (data.poderes == poderes) {
+                poderes += '<div class="tab-pane fade show' + (control ? ' active' : '') + '" id="list_' + UUID + '"' +
+                    'role="tabpanel" aria-labelledby="list_' + UUID + '-list">'
                 val.permissao.map((val2) => {
-                    if (val2 == valAntQue[1]) {
-                        data[i].permissao.splice([j], 1)
-                    }
-                    j++;
-                })
+                    poderes += '<div class="row"><div class="col-5">' +
+                        '<select id="selectId_' + UUID + '" class="form-control" disabled>' +
+                        '<option value="editar" ' + (val2 === "editar" ? 'selected' : '') + '>editar</option>' +
+                        '<option value="excluir" ' + (val2 === "excluir" ? 'selected' : '') + '>excluir</option>' +
+                        '<option value="incluir" ' + (val2 === "incluir" ? 'selected' : '') + '>incluir</option>' +
+                        '<option value="visualizar" ' + (val2 === "visualizar" ? 'selected' : '') + '>visualizar</option>' +
+                        '</select></div >' +
+                        '<div class="col-1 botaoum">' +
+                        '<button class="btn btn-outline-primary editar" type="button data-toggle="tooltip" data-placement="top" title="Deletar opção ' + val2 + '""><i class="las la-pen" style="font-size: 25px"></i></button>' +
+                        '</div>' +
+                        '<div class="col-1 botaodois">' +
+                        '<button class="btn btn-outline-danger remover" type="button data-toggle="tooltip" data-placement="top" title="Deletar opção ' + val2 + '""><i class="las la-trash-alt" style="font-size: 25px"></i></button>' +
+                        '</div></div>';
+                });
+                poderes += '<div class="col-md-4 offset-md-6">' +
+                    '<button class="btn btn-outline-primary adicionar" type="button data-toggle=" tooltip="" data-placement="top" title="Deletar opção visualizar"> <i class="las la-plus" style="font-size: 25px"></i></button>' +
+                    '</div>'
+                poderes += '</div>';
             }
-            i++;
+            control = false;
         });
-    });
-
-    $('.adicionar').click(function () {
-        $('#list-tab a').each(function (index) {
-            if ($(this).hasClass('active')) {
-                grupo_Valor = $(this).text();
-            }
-        });
-        var adicionar = $(this).attr("adicionar");
-        $('.adicionar').hide();
-        $('.bloquearLink').css("pointer-events", "none");
-        $("#" + adicionar).prepend('<div class="row sendoModificado" id="editarEsseId_' + grupo_Valor + '_' + selecionado + '"><div class="col-5">' +
-            '<select id="selectId_' + grupo_Valor + '" class="form-control" name="' + grupo_Valor + '">' +
-            '<option value="" data-default disabled selected></option>' +
-            '<option value="editar">editar</option>' +
-            '<option value="excluir">excluir</option>' +
-            '<option value="incluir">incluir</option>' +
-            '<option value="visualizar">visualizar</option>' +
-            '</select></div >' +
-            '<div class="col-1 botaoum">' +
-            '<button class="btn btn-success confirmar" valor="' + grupo_Valor + '" data-toggle="tooltip" data-placement="top"><i class="lar la-check-circle" style="font-size: 25px"></i></button>' +
-            '</div>' +
-            '<div class="col-1 botaodois">' +
-            '<button class="btn btn-danger cancelar" cancelar="' + grupo_Valor + '" data-toggle="tooltip" data-placement="top"><i class="las la-ban" style="font-size: 25px"></i></button>' +
-            '</div></div>');
-        controleCancelar();
-    });
-
-    $('.tab-content').on('click', '.cancelar', function () {
-        valorData = $(this).attr("valorData");
-        $('#valorData').remove();
-        $('.adicionar').removeAttr("style");
-        $(".bloquearLink").css("pointer-events", "");
-
-    });
-
-    $('.tab-content').on('click', '.confirmar', function () {
-        if (selecionado == "") {
-            alert("Selecione uma permissão");
-        } else {
-            $('.sendoModificado').attr('id', 'editarEsseId_' + grupo_Valor + '_' + selecionado);
-            $('.confirmar').attr('valorData', 'editarEsseId_' + grupo_Valor + '_' + selecionado);
-            $('.cancelar').attr('valorData', 'editarEsseId_' + grupo_Valor + '_' + selecionado);
-            $('.confirmar').attr('selectId', 'selectId_' + grupo_Valor + '_' + selecionado);
-            $('.cancelar').attr('selectId', 'selectId_' + grupo_Valor + '_' + selecionado);
-            $(".confirmar").toggleClass('btn-success btn-outline-primary');
-            $(".confirmar").html('<i class="las la-pen" style="font-size: 25px"></i>');
-            $(".confirmar").toggleClass('confirmar editarNovo');
-            $(".cancelar").toggleClass('btn-danger btn-outline-danger');
-            $(".cancelar").html('<i class="las la-trash-alt" style="font-size: 25px"></i>');
-            $(".cancelar").toggleClass('cancelar removerNovo');
-            $("#selectId_" + grupo_Valor).prop('disabled', true);
-            $('.adicionar').removeAttr("style");
-            $(".bloquearLink").css("pointer-events", "");
-            $(".sendoModificado").toggleClass('sendoModificado modificado');
-            $('#selectId_' + grupo_Valor).attr('id', 'selectId_' + grupo_Valor + '_' + selecionado);
-            data.map((val) => {
-                if (val.grupo == grupo_Valor) {
-                    val.permissao.push(selecionado);
-                }
-
-            });
-            console.log(data);
-            testeArray = selecionado;
-        }
-    });
-
-    $('.tab-content').on('click', '.editarNovo', function (event) {
-        if ($(this).is(".btn-outline-primary")) {
-            valorData = $(this).attr("valorData");
-            selectId = $(this).attr("selectId");
-            $(this).html('<i class="las la-check-double" style="font-size: 25px"></i>');
-            $(this).toggleClass('btn-outline-primary btn-outline-success');
-            $("#" + selectId).prop('disabled', false);
-        } else {
-            var i = 0;
-            var j = 0;
-            $('#' + valorData).attr('id', 'editarEsseId_' + grupo_Valor + '_' + selecionado);
-            $(this).attr('valorData', 'editarEsseId_' + grupo_Valor + '_' + selecionado);
-            $(this).html('<i class="las la-pen" style="font-size: 25px"></i>');
-            $(this).toggleClass('btn-outline-success btn-outline-primary');
-            $("#" + selectId).prop('disabled', true);
-            data.map((val) => {
-                if (val.grupo == grupo_Valor) {
-                    val.permissao.map((val2) => {
-                        if (val2 == selecionado) {
-                            data[i].permissao[j] = selecionado;
-                        } if (val2 == testeArray) {
-                            data[i].permissao[j] = selecionado;
-                        }
-                        j++;
-                    })
-                }
-                i++;
-            });
-            console.log(data);
-            testeArray = selecionado;
-        }
-    });
-
-    $('.tab-content').on('click', '.removerNovo', function () {
-        var i = 0;
-        var j = 0;
-        selectId = $(this).attr("selectId");
-        var removerArrayNovo = $("#" + selectId + " option:selected").val();
-        valorData = $(this).attr("valorData");
-        novoData = valorData.split("_");
-        $('#' + valorData).remove();
-        data.map((val) => {
-            if (val.grupo == grupo_Valor) {
-                val.permissao.map((val2) => {
-                    if (val2 == removerArrayNovo) {
-                        data[i].permissao.splice([j], 1)
-                    }
-                    j++;
-                })
-            }
-            i++;
-        });
-    });
-
-    $('.tab-content').on('change', 'select', function () {
-        selecionado = $(this).val();
-        valAntQue = valorAnterior.split("_");
-        data.map((val) => {
-            if (val.grupo == grupo_Valor) {
-                val.permissao.map((val2) => {
-                    if (val2 == selecionado) {
-                        $("button[valor='" + valorAnterior + "']").removeClass('btn-outline-success');
-                        $("button[valor='" + valorAnterior + "']").addClass('btn-outline-primary');
-                        $("button[valor='" + valorAnterior + "']").html('<i class="las la-pen" style="font-size: 25px"></i>');
-                        $(this).prop('disabled', true);
-                        $("option[value='" + valAntQue[1] + "']", this).prop('selected', true);
-                        alert("A função " + selecionado + " já está cadastrada nesse grupo!");
-                    }
-
-                    if ($('.modificado').is(".modificado")) {
-                        if (val2 == selecionado) {
-                            $(this).prop('disabled', true);
-                            $("option[value='" + testeArray + "']", this).prop('selected', true);
-                            $("button[valor='" + valorAnterior + "']").removeClass('btn-outline-success');
-                            $("button[valor='" + valorAnterior + "']").addClass('btn-outline-primary');
-                            $(".editarNovo").html('<i class="las la-pen" style="font-size: 25px"></i>');
-                        }
-                    }
-
-                    if ($('.sendoModificado').is(".sendoModificado")) {
-                        if (val2 == selecionado) {
-                            $('.sendoModificado').remove();
-                            $('.adicionar').removeAttr("style");
-                            $('.bloquearLink').css("pointer-events", "");
-                        }
-                    }
-                })
-            }
-        })
-    });
+        $('#list-tab').html(html);
+        $('#nav-tabContent').html(poderes);
+    }
 });
 
-function controleCancelar() {
-
-
-    selecionado = ''
+function create_UUID() {
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
 }
